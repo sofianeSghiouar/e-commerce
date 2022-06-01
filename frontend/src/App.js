c9408790b863ef116dad1f1d97951a642ca54e04
sofianeSghiouar <sofianesgh@gmail.com>
2022-06-01 19:16:56 +0200

    frontend the good deals: first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div >
+      <header>
+      <a href="/">The Good Deals</a>
+      </header>
+
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
