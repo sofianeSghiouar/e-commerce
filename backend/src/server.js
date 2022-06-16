ff0c445e6a71fcbf43f7dab249583264e111086f
sofianeSghiouar <sofianesgh@gmail.com>
2022-06-16 19:50:25 +0200

    ci: backend, graphql config

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,14 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import start from './db/connect.js';
+// const { ApolloServer, gql } = require('apollo-server');
+
 dotenv.config();
 
 import productsRouter from './controllers/products/routes.js';
 import usersRouter from './controllers/users/routes.js';
 import seedRouter from './controllers/seed/route.js';
+import { ApolloServer } from 'apollo-server-express';
 
 const app = express();
 
@@ -21,6 +24,15 @@ app.use('/api', seedRouter);
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// })
+
+// server.listen().then({url}, () => {
+//   console.log(`server started at : ${url}`);
+// })
 
 const PORT = process.env.PORT || 8000;
 
