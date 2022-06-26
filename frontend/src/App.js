8e0fe4b4ebb89b7ab5a69ca0c2b1849f3c576ae9
sofianeSghiouar <sofianesgh@gmail.com>
2022-06-26 09:03:33 +0200

    test husky

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,17 +15,12 @@ import { Store } from './Store';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
+import ShippingAddressPage from './pages/ShippingAddressPage';
+import PaymentPage from './pages/PaymentPage';
 
-
-
-
-
-          import CartPage from './pages/CartPage';
-          import LoginPage from './pages/LoginPage';
-          import ShippingAddressPage from './pages/ShippingAddressPage';
-          import PaymentPage from './pages/PaymentPage';
-
-          function App() {
+function App() {
   const { state, dispatch: storeDispatch } = useContext(Store);
   const { cart, userInfo } = state;
 
