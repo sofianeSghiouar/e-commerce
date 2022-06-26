067f105e9fc0121a085185bdbbc3e170bfd26faf
sofianeSghiouar <sofianesgh@gmail.com>
2022-06-26 08:59:55 +0200

    test husky

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,12 +13,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api', productsRouter);
 app.use('/api', usersRouter);
-
 app.use('/api', seedRouter);
 
 app.use((err, req, res, next) => {
