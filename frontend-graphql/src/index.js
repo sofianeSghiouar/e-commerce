d0ae59671e98e24e5bf1f9b407f9c43d03e13357
sofianeSghiouar <sofianesgh@gmail.com>
2022-06-26 04:09:07 +0200

    ci: cleaning

--- frontend-graphql/src/index.js
+++ frontend-graphql/src/index.js
@@ -17,16 +17,13 @@ const client = new ApolloClient({
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StoreProvider>
-    <HelmetProvider>
-      <ApolloProvider client={client}>
+  <ApolloProvider client={client}>
+    <StoreProvider>
+      <HelmetProvider>
         <App />
-      </ApolloProvider>
-    </HelmetProvider>
-  </StoreProvider>
+      </HelmetProvider>
+    </StoreProvider>
+  </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- frontend-graphql/src/pages/HomePage.js
+++ frontend-graphql/src/pages/HomePage.js
@@ -1,5 +1,3 @@
-// import React, { useEffect, useReducer } from 'react';
-// import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import Container from 'react-bootstrap/esm/Container';
@@ -9,76 +7,12 @@ import { useQuery } from '@apollo/client';
 import Product from '../components/Product';
 import Loading from '../components/Loading';
 import Message from '../components/Message';
-// import queryFetch from '../utils/queryHandler';
-
-// const reducer = (state, action) => {
-//   switch (action.type) {
-// case 'FETCH_REQUEST':
-//   return { ...state, loading: true };
-// case 'FETCH_SUCCESS':
-// return { ...state, products: action.payload };
-// return { ...state, products: action.payload, loading: false };
-// case 'FETCH_FAIL':
-//   return { ...state, loading: false, error: action.payload };
-//     default:
-//       return state;
-//   }
-// };
 import Queries from '../utils/graphql/gqlQueries';
 
 
 function HomePage() {
   const gqlQuery = new Queries();
   const { loading, error, data } = useQuery(gqlQuery.GET_PRODUCTS);
-
-  // useEffect(() => {
-  //   // const fetchProducts = async () => {
-  //   //   dispatch({ type: 'FETCH_REQUEST' });
-  //   //   const queryOptions = {
-  //   //     query: `
-  //   //     query{
-  //   //       getProducts {
-  //   //         id
-  //   //         name
-  //   //         slug
-  //   //         image
-  //   //         images
-  //   //         brand
-  //   //         category
-  //   //         description
-  //   //         price
-  //   //         countInStock
-  //   //         rating
-  //   //         numReviews
-  //   //         reviews {
-  //   //           createdAt
-  //   //         }
-  //   //         createdAt
-  //   //       }
-  //   //     }
-  //   //       `,
-  //   //   };
-  //   //   queryFetch(queryOptions)
-  //   //     .then((result) => {
-  //   //       if (result.hasOwnProperty('data')) {
-  //   //         console.log('result :>> ', result);
-  //   //         const {
-  //   //           data: {
-  //   //             data: { getProducts },
-  //   //           },
-  //   //         } = result;
-  //   //         if (getProducts) {
-  //   //           return dispatch({ type: 'FETCH_SUCCESS', payload: getProducts });
-  //   //         }
-  //   //       }
-  //   //       throw new Error(result.message);
-  //   //     })
-  //   //     .catch((error) => {
-  //   //       dispatch({ type: 'FETCH_FAIL', payload: error.message });
-  //   //     });
-  //   // };
-  //   // fetchProducts();
-  // }, []);
 
 
   return (
