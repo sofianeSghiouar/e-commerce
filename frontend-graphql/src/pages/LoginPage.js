663bf1ed7c03819d5fa2b7b9be7fd7dae429d3e3
sofianeSghiouar <sofianesgh@gmail.com>
2022-06-28 14:19:13 +0200

    test husky

--- frontend-graphql/src/pages/LoginPage.js
+++ frontend-graphql/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+    import React, { useContext, useEffect, useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap/esm';
 import { Helmet } from 'react-helmet-async';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
