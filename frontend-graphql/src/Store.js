b37bf865acc8cc30ad4de2e788d8533eb2f5a714
sofianeSghiouar <sofianesgh@gmail.com>
2022-07-13 13:25:47 +0200

    ci: graphql front update

--- frontend-graphql/src/Store.js
+++ frontend-graphql/src/Store.js
@@ -9,7 +9,10 @@ const initialState = {
       : {},
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
-      : []
+      : [],
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : ''
   },
   userInfo: localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
@@ -44,7 +47,12 @@ function reducer(state, action) {
     case 'USER_LOGOUT': {
       return {
         ...state,
-        cart: { ...state.cart, cartItems: [], shippingAddress: {} },
+        cart: {
+          ...state.cart,
+          cartItems: [],
+          shippingAddress: {},
+          paymentMethod: ''
+        },
         userInfo: null
       };
     }
@@ -52,6 +60,12 @@ function reducer(state, action) {
       return {
         ...state,
         cart: { ...state.cart, shippingAddress: action.payload }
+      };
+    }
+    case 'SAVE_PAYMENT_METHOD': {
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload }
       };
     }
     default:
