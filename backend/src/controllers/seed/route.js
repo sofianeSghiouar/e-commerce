551d3bd828e96327034c9194c7c011d8730e5756
sofianeSghiouar <sofianesgh@gmail.com>
2022-07-14 18:39:33 +0200

    ci: update backend seed route

--- backend/src/controllers/seed/route.js
+++ backend/src/controllers/seed/route.js
@@ -4,8 +4,13 @@ import { createRouter } from "../../utils/createRouter.js";
 const seedController = new SeedController();
 const routes = [
   {
+<<<<<<< HEAD
     path: "/seed/reset",
     method: "GET",
+=======
+    path: '/seed/reset',
+    method: 'GET',
+>>>>>>> ec17d9b (ci: update backend seed route)
     handler: seedController.resetDB.bind(seedController)
   }
 ];
